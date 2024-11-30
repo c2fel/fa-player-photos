270c668bf0f35f8c0494c508058d3ce80f27eadc
Christoph Zweifel <christoph.zweifel@student.unisg.ch>
2024-11-30 16:03:22 +0100

    made preparations for aruco markers

---
+++ training/train.py
@@ -0,0 +1,12 @@
+
+# the images in training/images are selected images from google drive containing aruco markes
+
+# read images
+
+# detect aruco markes
+
+# classify whether images has been taken on left or right court
+
+# process images
+
+# send images to customers based on checkin date
